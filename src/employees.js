25ef54a421693aa68f7e49f79d98a96e4905142a
Chris Kalama <chriskalama96@gmail.com>
2020-11-19 22:32:29 -0600

    refactored hooks, also began writing sortNames function

---
+++ src/employees.js
@@ -0,0 +1,14 @@
+
+
+const employees =
+[
+    {name: 'jay', email: 'jay@jay.com', DOB:'01.01.1990'}, 
+    {name: 'chris', email: 'chris@chris.com', DOB:'02.16.1996'},
+    {name: 'tyler', email: 'tyler@tyler.com', DOB:'02.02.1991'}
+
+]
+
+
+
+export default employees;
+
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,40 +3,56 @@ import '../App.css';
 import react, {useState} from "react";
 import Header from "../components/Header";
 import Body from "../components/Body";
+import employees from "../employees";
 
 
 function HomePage() {
+  console.log("THESE ARE OUR EMPLOYEES" , employees)
 
-  const [state, setState] = useState({
-    emps: [{name: 'jay', email: 'jay@jay.com', DOB:'01.01.1990'}, {name: 'chris', email: 'chris@chris.com', DOB:'02.16.1996'},{name: 'tyler', email: 'tyler@tyler.com', DOB:'02.02.1991'}],
-    filteredEmps: []
-  })
+  //Need to delete, not with hooks completely.
+  // const [state, setState] = useState({
+  //   emps: [{name: 'jay', email: 'jay@jay.com', DOB:'01.01.1990'}, {name: 'chris', email: 'chris@chris.com', DOB:'02.16.1996'},{name: 'tyler', email: 'tyler@tyler.com', DOB:'02.02.1991'}],
+  //   filteredEmps: []
+  // })
+
+  const [employeeState, setEmployeeState] = useState(employees)
+
+  const [filteredEmployeeState, setFilteredEmployeeState] = useState([])
+
+  function sortNames() {
+    // I need to sort by the name key in each object.
+    let sortEmployees = employeeState.sort();
+    setEmployeeState(sortEmployees)
+  
+  }
 
 
   const handleSearch = (event) => {
     console.log("WE ARE TYPING!",event.target.value)
-    var newFiltered = []
+    let newFiltered = []
 
-    for (let i =0; i<state.emps.length; i++) {
+    for (let i =0; i<employeeState.length; i++) {
       //console.log(state.emps[i].name)
 
       //the event.target.value is waiting for the state.emps.name.substring(which is a JS function to wait for how many letters the user types into the search bar. Has a couple cool features that can limit what is being typed or displayed based on that function) If it is at the "length" of the value in the search bar, then it will push into the empty array newFiltered)
-      if(event.target.value === state.emps[i].name.substring(0, event.target.value.length)){
-        newFiltered.push(state.emps[i]);
+      if(event.target.value === employeeState[i].name.substring(0, event.target.value.length)){
+        newFiltered.push(employeeState[i]);
       }
     }
 
     //console.log('new filtered ppeps to disply', newFiltered)
     //...state is taking a copy of the original state and then pushing what info. in this case it is filteredEmps with newFiltered.
     //Once pushed into the new array, we want to update the state, we can do that with the '...' and pushing the filteredEmps array from the state into the array we created in the function
-    setState({...state, filteredEmps: newFiltered})
+    setFilteredEmployeeState(newFiltered)
   }
 
-  console.log('STATE!!', state)
-  var empsToDisplay = state.emps
+  //We are testing to see what appears in the state, it is the original array and the pushed array based on what user types
+  console.log('STATE!!', filteredEmployeeState)
+  //Easy way to refactor state.emps into html. It is also in the map function. 
+  let empsToDisplay = employeeState
 
-  if (state.filteredEmps.length > 0){
-    empsToDisplay = state.filteredEmps
+  if (filteredEmployeeState.length > 0){
+    empsToDisplay = filteredEmployeeState
   }
 
 
@@ -47,8 +63,9 @@ function HomePage() {
       <Body />
 
     <input onChange={handleSearch}></input>
-    
-    <h1>Name</h1>
+    <br />
+    <br />
+    <button onClick={sortNames}>Sort Name A-Z</button>
 
 {/* Map over array like a loop. */}
   {empsToDisplay.map((emp)=>{
