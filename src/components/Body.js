6536dab07fc2bc08de32849de765a7b7483abf5c
Chris Kalama <chriskalama96@gmail.com>
2020-11-19 21:50:21 -0600

    Added psuedocode and Body component

---
+++ src/components/Body.js
@@ -0,0 +1,31 @@
+import react, {useState} from "react"
+import HomePage from "../pages/HomePage";
+import App from "../App";
+
+function Body() {
+
+
+return (
+    <div className="App">
+     
+
+
+    </div>
+
+
+
+
+);
+}
+
+
+
+
+
+
+
+
+
+
+
+export default Body;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,13 @@ import '../App.css';
 //Impotant Import
 import react, {useState} from "react";
 import Header from "../components/Header";
+import Body from "../components/Body";
 
 
 function HomePage() {
 
   const [state, setState] = useState({
-    emps: [{name: 'tom', email: 'tom@tom.com', DOB:'01.01.1990'}, {name: 'chris', email: 'chris@chris.com', DOB:'02.16.1996'},{name: 'tyler', email: 'tyler@tyler.com', DOB:'02.02.1991'}],
+    emps: [{name: 'jay', email: 'jay@jay.com', DOB:'01.01.1990'}, {name: 'chris', email: 'chris@chris.com', DOB:'02.16.1996'},{name: 'tyler', email: 'tyler@tyler.com', DOB:'02.02.1991'}],
     filteredEmps: []
   })
 
@@ -18,19 +19,18 @@ function HomePage() {
 
     for (let i =0; i<state.emps.length; i++) {
       //console.log(state.emps[i].name)
+
+      //the event.target.value is waiting for the state.emps.name.substring(which is a JS function to wait for how many letters the user types into the search bar. Has a couple cool features that can limit what is being typed or displayed based on that function) If it is at the "length" of the value in the search bar, then it will push into the empty array newFiltered)
       if(event.target.value === state.emps[i].name.substring(0, event.target.value.length)){
         newFiltered.push(state.emps[i]);
       }
-      
     }
 
-    console.log('new filtered ppeps to disply', newFiltered)
+    //console.log('new filtered ppeps to disply', newFiltered)
     //...state is taking a copy of the original state and then pushing what info. in this case it is filteredEmps with newFiltered.
+    //Once pushed into the new array, we want to update the state, we can do that with the '...' and pushing the filteredEmps array from the state into the array we created in the function
     setState({...state, filteredEmps: newFiltered})
   }
-
-
-
 
   console.log('STATE!!', state)
   var empsToDisplay = state.emps
@@ -44,23 +44,24 @@ function HomePage() {
   return (
     <div className="App">
       <Header />
+      <Body />
 
-      <input onChange={handleSearch}></input>
+    <input onChange={handleSearch}></input>
+    
+    <h1>Name</h1>
 
-      {/* Map over array like a loop. */}
+{/* Map over array like a loop. */}
+  {empsToDisplay.map((emp)=>{
 
-      <h1> Name!</h1>
-        {empsToDisplay.map((emp)=>{
-
-          return(
-            <div>
-              <h1>{emp.name}</h1>
-              <h1>{emp.email}</h1>
-              <h1>{emp.DOB}</h1>
-              {/* <h1>{emp.email}</h1> */}
-            </div>
-          )
-        })}
+    return(
+      <div>
+        <h1>{emp.name}</h1>
+        <h1>{emp.email}</h1>
+        <h1>{emp.DOB}</h1>
+        {/* <h1>{emp.email}</h1> */}
+      </div>
+    )
+  })}
     </div>
   );
 }
